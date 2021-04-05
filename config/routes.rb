59a97eef2ca65205a5c74a9d5408f39d6ec3ff1a
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-04-05 17:20:10 +0800

    generate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :stories, except: [ :destroy ]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
