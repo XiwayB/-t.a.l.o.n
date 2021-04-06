d5f1cbc3a7c0e5eca60424ca39ff076699d008b7
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-04-06 17:17:43 +0800

    debbug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 
 User.create!(
-  wechat_account: 'Xiway',
   role: 'patient',
-  location: '1234',
+  status: 'critical',
+  open_id: '1334',
+  location: {longitude: '113.937888', latitude: '22.524888'}
+  )
+
+User.create!(
+  role: 'medic',
   status: 'healthy',
-  open_id: '1234'
+  open_id: '1134',
+  location: {longitude: '113.937999', latitude: '22.524999'}
+  )
+
+User.create!(
+  role: 'patient',
+  status: 'non-critical',
+  open_id: '1234',
+  location: {longitude: '113.937777', latitude: '22.524777'}
   )
