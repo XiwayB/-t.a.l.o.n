20432db0a50540ad21058bb99968bb2f8c1ef3b1
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-04-06 11:18:05 +0800

    created the login action and set the wechat_user params in user_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::StoriesController < Api::V1::BaseController
+  URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
   before_action :set_user, only: [:show]
 
   def show; end
+
+  def login
+    @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+    render json: {
+      userId: @user.id
+    }
+  end
 
   private
 
@@ -16,5 +24,18 @@ class Api::V1::StoriesController < Api::V1::BaseController
   def render_error
     render json: { errors: @user.errors.full_messages },
       status: :unprocessable_entity
+  end
+
+
+  def wechat_user
+    wechat_params = {
+      appId: ENV["WECHAT_APP_ID"],
+      secret: ENV["WECHAT_APP_SECRET"],
+      js_code: params[:code],
+      grant_type: "authorization_code"
+    }
+
+    @wechat_response ||= RestClient.get(URL, params: wechat_params)
+    @wechat_user ||= JSON.parse(@wechat_response.body)
   end
 end
