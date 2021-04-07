d8dfb4d21aacdd2d627616086130e34485bd800b
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-04-07 09:51:12 +0800

    added the login route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, except: [ :destroy ]
+      post '/login', to: 'users#login'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
