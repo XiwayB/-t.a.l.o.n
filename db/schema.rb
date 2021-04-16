2b377718b055703087e46e1e30c56a08cd617bb0
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-04-16 10:31:53 +0800

    added avatar and description to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_035344) do
+ActiveRecord::Schema.define(version: 2021_04_16_022648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_04_13_035344) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "open_id"
+    t.text "avatar"
+    t.text "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
